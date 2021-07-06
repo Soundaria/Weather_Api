0455ad42cee82d54037cae438ce14de3b8507a31
Soundaria <78199061+Soundaria@users.noreply.github.com>
2021-07-06 13:30:06 +0530

    responsive for mobile

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ async function weather(){
     var x="http://api.weatherapi.com/v1/current.json?key=f68b420733c844d6ab072140212806&q="+city+"&aqi=yes";
     var weather=await fetch(x);
     var res=await weather.json();
-    var report=res.current.temp_c;
-    document.getElementById("result").innerHTML=report;
+    if(city!="choose"){
+        document.getElementById("section1").style.display="none";
+        document.getElementById("section2").style.display="none";
+        document.getElementById("result").innerHTML=res.location.name;
+        document.getElementById("temp").innerHTML="Temperature : "+res.current.temp_c+"°C // "+res.current.temp_f+" F<br /> Climate :  "+res.current.condition.text+"<br /> Humidity : "+res.current.humidity+" g/cubicmeter <br /> Latitude : "+res.location.lat+"°<br /> Longitude : "+res.location.lon+"°";
+    }
+    else{
+        alert("Place field cannot be empty");
+    }
 }
