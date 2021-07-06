097f13f8af126c0eac1f052707d18cc6571de924
Soundaria <78199061+Soundaria@users.noreply.github.com>
2021-07-06 21:35:27 +0530

    responsive all pixels

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ async function weather(){
     var x="http://api.weatherapi.com/v1/current.json?key=f68b420733c844d6ab072140212806&q="+city+"&aqi=yes";
     var weather=await fetch(x);
     var res=await weather.json();
+    var scr_width=window.innerWidth;
     if(city!="choose"){
         document.getElementById("section1").style.display="none";
         document.getElementById("section2").style.display="none";
         document.getElementById("result").innerHTML=res.location.name;
-        document.getElementById("temp").innerHTML="Temperature : "+res.current.temp_c+"°C // "+res.current.temp_f+" F<br /> Climate :  "+res.current.condition.text+"<br /> Humidity : "+res.current.humidity+" g/cubicmeter <br /> Latitude : "+res.location.lat+"°<br /> Longitude : "+res.location.lon+"°";
+        document.getElementById("temp").innerHTML="Temperature : "+res.current.temp_c+"°C // "+res.current.temp_f+" F<br /> Climate :  "+res.current.condition.text+"<br /> Humidity : "+res.current.humidity+" g/m³  <br /> Latitude : "+res.location.lat+"°<br /> Longitude : "+res.location.lon+"°";
     }
     else{
-        alert("Place field cannot be empty");
+        alert("Input field cannot be empty");
     }
+    var temp=res.current.temp_c;
+    if(temp>=30){
+        document.getElementById("sunny").style.display="block";
+    }
+    else if(temp>=20){
+        document.getElementById("sunny").style.display="block";
+    }
+
 }
